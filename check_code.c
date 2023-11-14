9b9085664c2bf2ad61599acb1f3360ab37239136
vlad <vladen.bokovoi@mail.ru>
2023-11-14 15:08:53 +0300

    wd

--- check_code.c
+++ check_code.c
@@ -3,27 +3,34 @@
 
 int GetUserChoice(void)
 {
-	int  input = 0;
-	while (true)
-	{
-		while (!scanf("%d", &input)) {
-			while (getchar() != '\n');
-		}
-		while (getchar() != '\n');
-		if (input == 1 || input == 2)
-		{
-			break;
-		}
-		printf("Ошибка ввода. Введите вещественное число.\n");
-	}
-	return input;
+    char temprem;
+    int input = 0;
+    while (true)
+    {
+        temprem = 0;
+        if ((!scanf("%d%c", &input, &temprem)) || temprem != '\n')
+        {
+            printf("Ошибка ввода.\nВведите снова: ");
+            while (getchar() != '\n');
+        }
+        else if (input == 1 || input == 2) {
+            return input;
+        }
+
+    }
 }
 double GetDouble(void) { //функция проверки ввода вещественного числа
-	double input = 0.0;
-	while (!scanf("%lf", &input)) {
-		while (getchar() != '\n');//очистка потока ввода
-		printf("Ошибка ввода. Введите вещественное число.\n");
-	}
-	while (getchar() != '\n');//Повторная чистка потока(даже при успешном вводе)
-	return input;
+    char temprem;
+    double input = 0;
+    while (true)
+    {
+        temprem = 0;
+        if ((!scanf("%lf%c", &input, &temprem)) || temprem != '\n')
+        {
+            printf("Ошибка ввода.\nВведите снова: ");
+            while (getchar() != '\n'); //чистим поток ф-цей getchar()
+        }
+        else
+            return input;
+    }
 }
